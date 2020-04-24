a2b67f966b286e9bd7e38d71f0ffb352a138c244
Colin Osborn <colin.osborn@gmail.com>
2020-04-23 22:51:27 -0600

    test for contact page

--- spec/features/user_wants_to_find_out_about_me_spec.rb
+++ spec/features/user_wants_to_find_out_about_me_spec.rb
@@ -10,5 +10,6 @@ RSpec.feature "User wants to found out about me" do
         expect(page).to have_content "About" 
         expect(page).to have_content "Colin Osborn"
         expect(page).to have_content "Web Developer"
+        expect(page).to have_content "I am full stack web developer. I live in the mountains of Colorado where I enjoy mountain biking and skiing."
     end
 end
