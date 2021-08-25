355076149b6ab713e6ee46815db7f25462352233
colin <cosborn@patientslikeme.com>
2021-08-25 12:11:29 -0600

    created technologies model to reference portfolios

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_19_031612) do
+ActiveRecord::Schema.define(version: 2021_08_25_180625) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,4 +32,13 @@ ActiveRecord::Schema.define(version: 2021_08_19_031612) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "technologies", force: :cascade do |t|
+    t.string "name"
+    t.bigint "portfolios_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["portfolios_id"], name: "index_technologies_on_portfolios_id"
+  end
+
+  add_foreign_key "technologies", "portfolios", column: "portfolios_id"
 end
